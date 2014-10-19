1a6f18a9b202f20c9b64e90b1bea4029af212cf0
Manthan Dave <manthanhd@live.com>
2014-10-19 01:01:48 +0100

    Linear curve logic added.

--- caller.js
+++ caller.js
@@ -3,21 +3,56 @@ var util = require('util')
 
 var deviceIDs = ["tiny_disruptor", "little_giant", "nimble_sprout", "shiny_seed", "saucy_root", "kingly_leaf", "curly_thistle", "picky_prickle", "thorny_velvet", "springy_pollen"];
 var baseInterval = 1000;
-var totalRequests = 672;
+var requestCount = 1;
 
-deviceIDs.forEach(function(deviceID){
-  console.log(deviceID);
-});
 
-var random = function (low, high) {
-    return Math.random() * (high - low) + low;
-}
+var totalRequests = 672*deviceIDs.length;
+
+// ------------------------- FUNCTION DEFINITIONS END -------------------------
 
 var getJson = function(){
   var file = '/home/root/data.json'
   jf.readFile(file, function(err, obj) {
-    console.log(obj);
-    report(obj.temp, obj.light);
+    //console.log(obj);
+    var dayRequest = 1;
+    var rising = true;
+    for(var dayRequestCount = 0; dayRequestCount <= 9; dayRequestCount++){
+      var pon = random(1,5);
+      var differenceTemp = 0, differenceLight = 0;
+      if((5-pon) > (pon-1)){
+        differenceTemp = random(-2, 0);
+        differenceLight = random(-5, 0);
+      } else {
+        differenceTemp = random(0, 2);
+        differenceLight = random(0, 5);
+      }
+      obj.temp = (obj.temp - 0) + (differenceTemp - 0);
+      obj.light = (obj.light - 0) + (differenceLight - 0);
+
+      if(rising == true){
+        obj.temp += 2;
+        obj.light += 10;
+        dayRequest++;
+      } else {
+        obj.temp -= 2;
+        obj.light -= 10;
+        dayRequest--;
+      }
+
+      if(dayRequest == 5) {
+        rising = false;
+      } else if (dayRequest == 0) {
+        rising = true;
+      }
+
+      console.log("\t\tApplying differenceLight: " + differenceLight + ", differenceTemp: " + differenceTemp);
+      console.log("\t\tTemp: " + obj.temp + ", Light: " + obj.light + (rising == true ? " RISING" : " LOWERING"));
+      console.log("dayRequest:" + dayRequest);
+      requestCount++;
+    }
+    console.log("requestCount:" + requestCount);
+    
+    //report(obj.temp, obj.light);
   });
 };
 
@@ -41,3 +76,19 @@ var report = function(temperature, light) {
     });
   }).end();
 };
+
+// ------------------------- FUNCTION DEFINITIONS END -------------------------
+
+deviceIDs.forEach(function(deviceID){
+  console.log("Current request count: " + requestCount);
+
+  console.log("\tPosting as " + deviceID);
+  getJson();
+
+});
+
+var random = function (low, high) {
+    return Math.random() * (high - low) + low;
+}
+
+
